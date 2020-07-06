236e62bde596da31315a73986cc31a767293525d
kz <zaramaluna1999@hotmail.com>
2020-07-06 16:01:35 -0500

    Updates

---
+++ precache-manifest.2b8d3ac740104eb42e932ed13e4879e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef9eb1f55f58e813ae4a5160de12cc41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39b009aa428b9c4db73e",
+    "url": "/static/css/main.4c0c3471.chunk.css"
+  },
+  {
+    "revision": "1346261579c0d743ebd7",
+    "url": "/static/js/2.bc360008.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bc360008.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39b009aa428b9c4db73e",
+    "url": "/static/js/main.2f09891e.chunk.js"
+  },
+  {
+    "revision": "556a9fbc7d787efccce5",
+    "url": "/static/js/runtime-main.061d3831.js"
+  },
+  {
+    "revision": "1c09753976d51dbb399bec4774fb5fa1",
+    "url": "/static/media/wolf.1c097539.png"
+  }
+]);
